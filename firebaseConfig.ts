8b9885fed36e6eae979975862b55184eb322eccf
100kNow <100know.official01@gmail.com>
2025-04-28 11:32:21 +0100

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -11,13 +11,13 @@ import {
 import { getStorage }               from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey:            'AIzaSyCq6QQAzeAjgbd5bYHwdn6Ebe1MlLHTdmA',
-  authDomain:        'know-b4cee.firebaseapp.com',
-  projectId:         'know-b4cee',
-  storageBucket:     'know-b4cee.appspot.com',
-  messagingSenderId: '1013086836066',
-  appId:             '1:1013086836066:web:e111ad7bc3f6e74b71483b',
-  measurementId:     'G-YQZD5C4XDF',
+  apiKey:            '',
+  authDomain:        '',
+  projectId:         '',
+  storageBucket:     '',
+  messagingSenderId: '',
+  appId:             '',
+  measurementId:     '',
 };
 
 /** one (and only one) Firebase app */
